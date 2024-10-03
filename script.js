4a972f470256364f226abb95ecff34d8c29ffa94
Abdul Rehman <abdulrehmang571@gmail.com>
2024-10-03 08:33:08 +0500

    fixed js and added checkbox

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-button = document.querySelector('button');
-password = document.querySelector('#password');
-confirmPassword = document.querySelector('#confirm-password');
-passwordMatch = document.querySelector('.password-match');
-button.addEventListener('click',(e)=>{
-    if(password.value !== confirmPassword.value){
+const button = document.querySelector('button');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const passwordMatch = document.querySelector('.password-match');
+
+button.addEventListener('click', (e) => {
+    if (password.value !== confirmPassword.value) {
         e.preventDefault();
         passwordMatch.style.display = 'block';
-        password.style.border = '1px solid red';
-        confirmPassword.style.border = '1px solid red';
+        password.style.outline = '1px solid red';
+        confirmPassword.style.outline = '1px solid red';
         password.value = '';
         confirmPassword.value = '';
-
     }
 });
